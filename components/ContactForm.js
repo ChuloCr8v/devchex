985cde2fdefd68d46184246dad471ed10768a876
chulocr8v <nkematu5@gmail.com>
2021-09-14 18:34:38 +0100

    update

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -11,7 +11,7 @@ const ContactForm = () => {
   
   return(
     <form name="contact" data-aos="fade-up" data-netlify="true" className={style.contactform}>
-    <input type="hidden" name="form-name" value="contact">
+    <input type="hidden" name="form-name" value="contact"/>
       <h5> Leave a message or enquiry </h5>
       <label htmlFor='name'> Name </label>
         <input type='text' required placeholder= 'Enter Name' />
